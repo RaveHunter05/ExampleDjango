8804b1b3fec9bbf51ad2637978ace7c2a4d6805a
RaveHunter05 <paulsotelo97@gmail.com>
2019-10-15 00:22:14 -0600

    trying xd

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from principal.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', post_list)
+
 ]
