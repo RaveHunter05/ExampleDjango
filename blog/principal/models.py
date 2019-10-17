d95a135158f7c53f49e70cc3fa142e5558b23e12
socratespalma <dubstepero10@gmail.com>
2019-10-17 13:57:02 -0600

    probando branch

--- blog/principal/models.py
+++ blog/principal/models.py
@@ -19,5 +19,92 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+#---------------------------------------- Personas, Temas, Album ---------------------------------
     
-    
+class Productor(models.Model):
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+    Nombre_artistico=models.CharField(max_length=50)
+
+class Cantante(models.Model):
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+    Nombre_artistico=models.CharField(max_length=50)
+
+class Genero(models.Model):
+    Nombre=models.CharField(max_length=30)
+    Descripcion=models.TextField(blank=True)
+
+class Album(models.Model):
+    Nombre=models.CharField(max_length=60)
+    Tipo_album=models.CharField(max_length=30)
+    Fecha_lanzamiento=models.DateField(auto_now=True)
+    '''Duracion=models.                     Investigar tipo de campo para manejar 
+                                                        los tiempos hh//mm//ss
+    '''
+    Estrellas=models.IntegerField() #Validar valor maximo entre 1-5
+
+class Tema(models.Model):
+    Genero=models.ForeignKey(Genero, on_delete=models.CASCADE)
+    Album=models.ForeignKey(Album, on_delete=models.CASCADE)
+    Titulo=models.CharField(max_length=60)
+    '''Duracion=models.                     Investigar tipo de campo para manejar 
+                                                        los tiempos hh//mm//ss
+    '''
+    Estrellas=models.IntegerField() #Validar valor maximo entre 1-5
+    Fecha_lanzamiento=models.DateField(auto_now=True)
+    Precio=models.DecimalField(max_digits=10, decimal_places=2)
+
+class Tema_Productor(models.Model):
+    Productor=models.ForeignKey(Productor, on_delete=models.CASCADE)
+    Tema=models.ForeignKey(Tema, on_delete=models.CASCADE)
+
+class Tema_Cantante(models.Model):
+    Cantante=models.ForeignKey(Cantante, on_delete=models.CASCADE)
+    Tema=models.ForeignKey(Tema, on_delete=models.CASCADE)
+
+class Album_Genero(models.Model):
+    Album=models.ForeignKey(Album, on_delete=models.CASCADE)
+    Genero=models.ForeignKey(Genero, on_delete=models.CASCADE)
+
+#------------------------------------------ Login y Autenticación -------------------------------------
+
+class Cliente(models.Model):
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+
+class Rol(models.Model):
+    Descripcion=models.CharField
+
+class Usuario(models.Model):
+    Rol=models.ForeignKey(Rol,on_delete=models.CASCADE)
+    Cliente=models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    Nombre_Usuario=models.CharField(max_length=50)
+    Contraseña=models.CharField(max_length=128)
+    Correo=models.EmailField(unique=True)
+    Estado=models.BooleanField(default=True)
+
+
+#------------------------------------ Venta de temas ---------------------------------------------------
+
+class Compra(models.Model):
+    Cliente=models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    Fecha_realizacion=models.DateField(auto_now=True)
+    Concepto=models.TextField(blank=True)
+    Monto=models.DecimalField(max_digits=10,decimal_places=2)
+
+class Detalle_Compra(models.Model):
+    Compra=models.ForeignKey(Compra, on_delete=models.CASCADE)
+    Tema=models.ForeignKey(Tema, on_delete=models.CASCADE)
+    Album=models.ForeignKey(Album, on_delete=models.CASCADE)
+    Fecha_realizacion=models.DateField(auto_now=True)
+    Cantidad=models.PositiveIntegerField()
+    Precio=models.DecimalField(max_digits=10,decimal_places=2)
+
+
+
+
+
+
+
