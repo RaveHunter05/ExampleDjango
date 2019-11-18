2c5486518702074494c925aebe2659f9077f7dad
RaveHunter05 <paulsotelo97@gmail.com>
2019-11-17 22:03:30 -0600

    ya funciona con postgress :D

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'facilito',
+        'USER': 'postgres', 
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- blog/principal/admin.py
+++ blog/principal/admin.py
@@ -4,5 +4,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Post)
-
+admin.site.register(productor)
 
