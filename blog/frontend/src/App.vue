6330a5581621cbb3a96f540715dd3d8f90d6eb52
RaveHunter05 <paulsotelo97@gmail.com>
2020-04-04 21:50:03 -0600

    haciendo cambios en git ignore

--- blog/frontend/src/App.vue
+++ blog/frontend/src/App.vue
@@ -42,14 +42,6 @@ export default {
 </script>
 
 <style>
-#app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
-}
 
 html, body{
    min-height: 100%;
